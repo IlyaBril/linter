c1da5366cd946071b3d0f7d14499957a309a68fa
ilyabrilev@yandex.ru <ilyabrilev@yandex.ru>
2024-12-08 15:02:13 +0300

    database delete

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite+aiosqlite:///../app/app.py.db"
+basedir = os.path.abspath(os.path.dirname(__file__))
+DATABASE_URL = "sqlite+aiosqlite:///" + os.path.join(basedir, "app.py.db")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 # expire_on_commit=False will prevent attributes from being expired
